80aedceafce44f276b332010266ed52c28c54c03
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-05-31 16:34:41 +0100

    Added examples folder

---
+++ examples/starter.js
@@ -0,0 +1,16 @@
+import {Maze} from "https://deno.land/x/maze_generator/MazeClasses.js"
+
+let mazeSettings = {
+  width: 12,
+  height: 12,
+  algorithm: "recursive backtracker"
+}
+
+//initialize the maze
+let m = Maze.create(mazeSettings);
+
+//generate it
+m.generate();
+
+
+console.log(JSON.stringify(m));
