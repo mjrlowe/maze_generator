c4702d92ed47298d7b2580dcc57818021839962c
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-06-01 17:34:29 +0100

    fixed line() function

--- display.js
+++ display.js
@@ -206,7 +206,10 @@ export default function display(maze, canvas) {
   }
 
   function line(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.lineJoin = "round";
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
+    ctx.stroke(); 
   }
 }
