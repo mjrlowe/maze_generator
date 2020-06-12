39378e1b961b60a25033dde478636ae48990d5d0
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-06-12 14:56:10 +0100

    0 should be string for hex code

--- display.js
+++ display.js
@@ -299,7 +299,7 @@ export default function display({
 
   function componentToHex(c) {
     var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+    return hex.length === 1 ? 0 + hex : hex;
   }
   
 }
