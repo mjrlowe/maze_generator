9bee9dfcb8e01451d2858cfb9938cb3acfcc5fdc
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-07-03 12:26:57 +0100

    maze widget shouldn't throw errors when pressing the buttons now

--- createWidget.js
+++ createWidget.js
@@ -6,26 +6,47 @@ export default function createWidget(settings) {
   let maze = Maze.create(settings);
 
   let mazeId = maze.algorithmId + "-" + maze.seed;
+  let widgetId = mazeId + "-widget";
+  let canvasId = mazeId + "-canvas";
   let styleWidth = widgetWidth ?? widgetSize ?? widgetHeight ?? "auto";
   let styleHeight = widgetHeight ?? widgetSize ?? widgetWidth ?? "auto";
   let paused = settings.paused ?? false;
 
   let html =
   `
-  <div class="maze-widget ${maze.algorithmId}" id="${mazeId}--widget" style="display: inline-block; text-align: center;">
-  <canvas width="220" height="220" style="width:${styleWidth}; height:${styleHeight}" class="maze-canvas" id="${mazeId}-canvas"></canvas>
+  <div class="maze-widget ${maze.algorithmId}" id="${widgetId}" style="display: inline-block; text-align: center;">
+  <canvas width="220" height="220" style="width:${styleWidth}; height:${styleHeight}" class="maze-canvas" id="${canvasId}"></canvas>
   <div class="maze-widget-options">
-    <button class="play-pause-button" onClick="paused=!paused">pause/play</button>
-    <button class="step-button" onClick="paused=true; maze.step()">step</button>
-    <button class="finish-button" onClick="maze.generate()">finish</button>
-    <button class="restart-button" onClick="maze.reset()">restart</button>
+    <button class="play-pause-button" onClick="document.getElementById(${widgetId}).playPauseMaze()">pause/play</button>
+    <button class="step-button" onClick="document.getElementById(${widgetId}).stepMaze()">step</button>
+    <button class="finish-button" onClick="document.getElementById(${widgetId}).generateMaze()">finish</button>
+    <button class="restart-button" onClick="document.getElementById(${widgetId}).restartMaze()">restart</button>
   </div>
 </div>
   `;
 
   document.body.innerHTML += html;
 
-  let canvas = document.getElementById(mazeId + "-canvas");
+  let widget = document.getElementById(widgetId);
+
+  widget.playPauseMaze = () => {
+    paused = !paused;
+  };
+
+  widget.stepMaze = () => {
+    maze.step();
+    paused = true;
+  };
+
+  widget.generateMaze = () => {
+    maze.generate();
+  };
+  
+  widget.restartMaze = () => {
+    maze.reset();
+  };
+
+  let canvas = document.getElementById(canvasId);
 
   maze.display({ canvas });
 
