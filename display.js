aaeb7bdd6d061cf75e3a52e6a5e5d2c1471a1c72
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-07-04 21:48:12 +0100

    fixed error 'entrance is not defined'

--- display.js
+++ display.js
@@ -33,14 +33,14 @@ export default function display({
   if(removeWallsAtEntranceAndExit){
     //if the entrance wall is a valid direction
     if(directions.indexOf(maze.entrance.direction) !== -1){
-      entranceWallBefore = maze.walls[entrance.y][entrance.x][entrance.direction]
-      maze.walls[entrance.y][entrance.x][entrance.direction] = false;
+      entranceWallBefore = maze.walls[maze.entrance.y][maze.entrance.x][maze.entrance.direction]
+      maze.walls[maze.entrance.y][maze.entrance.x][maze.entrance.direction] = false;
     }
 
     //if the exit wall is a valid direction
     if(directions.indexOf(maze.exit.direction) !== -1){
-      exitWallBefore = maze.walls[exit.y][exit.x][exit.direction]
-      maze.walls[exit.y][exit.x][exit.direction] = false;
+      exitWallBefore = maze.walls[maze.exit.y][maze.exit.x][exit.direction]
+      maze.walls[maze.exit.y][maze.exit.x][maze.exit.direction] = false;
     }
   }
 
@@ -256,12 +256,12 @@ export default function display({
   if(removeWallsAtEntranceAndExit){
     //re-add the entrance wall if it was taken away to begin with
     if(directions.indexOf(maze.entrance.direction) !== -1){
-      maze.walls[entrance.y][entrance.x][entrance.direction] = entranceWallBefore;
+      maze.walls[maze.entrance.y][maze.entrance.x][maze.entrance.direction] = entranceWallBefore;
     }
 
     //re-add the exit wall if it was taken away to begin with
     if(directions.indexOf(maze.exit.direction) !== -1){
-      maze.walls[exit.y][exit.x][exit.direction] = exitWallBefore;
+      maze.walls[maze.exit.y][maze.exit.x][maze.exit.direction] = exitWallBefore;
     }
   }
 
