642e3ae647681ec4c3dbb6f6e106196afe8c3bd4
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-07-19 22:11:54 +0100

    fixed bug 'imageButtons is not defined'

--- createWidget.js
+++ createWidget.js
@@ -35,7 +35,7 @@ export default function createWidget(mazeSettings={}, displaySettings=mazeSettin
   const cssFileURL = "https://x.nest.land/maze_generator@0.1.0-alpha.0/widget-styles.css";
 
   function getButtonInnerHTML(buttonName) {
-    if (imageButtons) {
+    if (settings.imageButtons) {
       return `<img 
         class="maze-widget-image maze-widget-${buttonName}-image"
         src="${iconImageFolderURL}/${buttonName}.svg" 
