8f5449502e67b47538249f516bb417a767f3f84c
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-10-13 18:18:54 +0100

    braid method no longer removes tries to remove walls for cells that are no longer dead ends

--- src/braid.js
+++ src/braid.js
@@ -6,14 +6,16 @@ import {
   dy
 } from "./directions.js";
 
-export default function braid({maze}) {
+export default function braid({
+  maze
+}) {
 
   let deadEnds = [];
   maze.walls.forEach((row, y) => {
     row.forEach((cell, x) => {
       let connectedDirections = [];
       for (let direction in cell) {
-        if(!cell[direction]) connectedDirections.push(direction);
+        if (!cell[direction]) connectedDirections.push(direction);
       }
       if (connectedDirections.length === 1) {
         deadEnds.push({
@@ -24,20 +26,34 @@ export default function braid({maze}) {
       }
     });
   });
-  
+
+  deadEnds = maze.shuffle(deadEnds);
+
   for (let deadEnd of deadEnds) {
-    let unconnectedNeighbors = [];
-    for (let direction of directions) {
-      let neighbor = {
-        x: deadEnd.x + dx[direction],
-        y: deadEnd.y + dy[direction]
-      }
-      if (direction != opposite[deadEnd.direction] && maze.cellIsInMaze(neighbor)) {
-        unconnectedNeighbors.push({...neighbor, direction});
-      }
+    let connections = 0;
+    for (let direction in maze.walls[deadEnd.y][deadEnd.x]) {
+      if (!maze.walls[deadEnd.y][deadEnd.x][direction]) connections++;
     }
 
-    let chosenNeighbor = unconnectedNeighbors[Math.floor(maze.random()*unconnectedNeighbors.length)];
-    maze.removeWall(deadEnd, chosenNeighbor.direction);
+    //if it still is a dead end
+    if (connections <= 1) {
+      let unconnectedNeighbors = [];
+      for (let direction of directions) {
+        let neighbor = {
+          x: deadEnd.x + dx[direction],
+          y: deadEnd.y + dy[direction]
+        }
+        if (direction != opposite[deadEnd.direction] && maze.cellIsInMaze(neighbor)) {
+          unconnectedNeighbors.push({
+            ...neighbor,
+            direction
+          });
+        }
+      }
+
+      let chosenNeighbor = unconnectedNeighbors[Math.floor(maze.random() * unconnectedNeighbors.length)];
+      maze.removeWall(deadEnd, chosenNeighbor.direction);
+    }
   }
+  return maze;
 }
