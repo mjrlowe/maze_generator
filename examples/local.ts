07a7d886cc6b1af097cac215b3228f8286a40aa5
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-12-05 18:52:21 +0000

    changed some lets to consts

--- examples/local.ts
+++ examples/local.ts
@@ -1,14 +1,14 @@
 import { Maze } from "../mod.js";
 
 // 20x20 maze using
-let mazeSettings = {
+const mazeSettings = {
   size: 20,
   algorithm: "growingtree",
   cellSelectionMethod: { newest: 1, random: 2, oldest: 7 },
 };
 
 //initialize the maze
-let m = new Maze(mazeSettings);
+const m = new Maze(mazeSettings);
 
 //generate it
 m.generate();
